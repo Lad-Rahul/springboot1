01dbe8585b06c271950b2a0819bdce2e17319d4c
Lad-Rahul <rahullad945@gmail.com>
2023-12-13 19:29:48 +0530

    Application properties file and value annotation

--- src/main/java/com/springboot1/controller/StudentController.java
+++ src/main/java/com/springboot1/controller/StudentController.java
@@ -1,8 +1,9 @@
 package com.springboot1.controller;
 
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -13,10 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/student")
 public class StudentController {
+	/*
+	 * To get value from application.properties
+	 * default value can be given so it will fallback to that 
+	 * if not found in properties file
+	 * Only works with spring manage beans, not with POJO
+	 */
+	@Value("${app.name:defaultValue}")
+	private String appName;
 	
 	@GetMapping("/get")
 //	@RequestMapping(value="/get", method=RequestMethod.GET)
 	public String getStudent() {
-		return "Hello World 2";
+		return appName;
 	}
 }
